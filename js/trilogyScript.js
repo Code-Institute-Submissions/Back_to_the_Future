4cde699248a299409c92850ba34157fe4c1d7562
Liam Cafearo <lcafearo@gmail.com>
2018-02-12 07:17:35 +0000

    Updating JavaScript to make it cleaner

--- js/trilogyScript.js
+++ js/trilogyScript.js
@@ -7,33 +7,33 @@ const BTTF3_IMDB_ID = "tt0099088"; // Back to the future 3
 const OMDB_URL = "https://www.omdbapi.com/?i=";
 const MOVIES = [BTTF1_IMDB_ID, BTTF2_IMDB_ID, BTTF3_IMDB_ID];
 
-console.log(MOVIES);
 $(document).ready(function() {
   // API requests from https://www.themoviedb.org/
   // Trilogy API scripts - The code for the API's was inspired by Simen Daehlin's guidance.
-  MOVIES.forEach(movie => {
-    fetch(OMDB_URL + movie + "&plot=full&apikey=" + OMDB_API_KEY)
-      .then(res => res.json())
-      .then(movieData => {
-        console.log(movieData);
-        let actors = movieData.Actors.split(",");
-        actors.forEach(function(actor) {
-          $("#actorBttf" + [i+1]).append("<li>" + actor + "</li>");
-        });
+      MOVIES.forEach((movie, i) => {
+        fetch(OMDB_URL + movie + "&plot=full&apikey=" + OMDB_API_KEY)
+          .then(res => res.json())
+          .then(movieData => {
+            console.log(movieData);
+            let actors = movieData.Actors.split(",");
+            actors.forEach((actor, i) => {
+              $("#actorBttf" + [i]).append("<li>" + actor + "</li>");
+            });
 
-        // Adds Movie Ratings
-
-        $("#reviews" + [i+1]).append(
+      // Adds Movie Ratings
+      movieData.Ratings.forEach((rating, i) => {
+        $("#reviews" + [i]).append(
           "<li>" +
             movieData.Ratings[i].Source +
             ":" +
             movieData.Ratings[i].Value +
             "</li>"
         );
+      })
 
-        $("#plotBttf" + [i+1]).html(movie.Plot);
+        $("#plotBttf" + [i]).html(movieData.Plot);
         // adds movie poster
-        $("#posterBttf" + [i+1]).attr("src", movie.Poster);
+        $("#posterBttf" + [i]).attr("src", movieData.Poster);
       })
       .catch(error => console.log(error));
   });
